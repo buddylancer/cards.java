d84381e1a327b7099448c2afd96ac1438eb54e75
buddy.lancer <buddy.lancer@gmail.com>
2021-01-25 10:35:03 +0300

    Add time tracking to run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,13 @@
+@echo off
 set JAVA_HOME=C:\opt\jdk1.8.0_251
 
 set folder=%1
 if "%folder%"=="" set folder=imgs
 
+set start=%time%
 %JAVA_HOME%\bin\java -jar java_test_task.jar %folder%
+set end=%time%
+
+echo Start=%start%; End=%end%.
 
 pause
